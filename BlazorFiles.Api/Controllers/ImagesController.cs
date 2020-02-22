cfd445b5e512b4cfce22bc85840da01ab33e5ef1
aksoftware98 <aksoftware98@hotmail.com>
2020-02-22 12:28:33 +0200

    Upload file to Server via API

---
+++ BlazorFiles.Api/Controllers/ImagesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace BlazorFiles.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+
+        private readonly IHostEnvironment _environment;
+
+        public ImagesController(IHostEnvironment environment)
+        {
+            this._environment = environment; 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm]IFormFile image)
+        {
+            if (image == null || image.Length == 0)
+                return BadRequest("Upload a file");
+
+            string fileName = image.FileName;
+            string extension = Path.GetExtension(fileName);
+
+            string[] allowedExtensions = { ".jpg", ".png", ".bmp" };
+
+            if (!allowedExtensions.Contains(extension))
+                return BadRequest("File is not a valid image");
+
+            string newFileName = $"{Guid.NewGuid()}{extension}";
+            string filePath = Path.Combine(_environment.ContentRootPath, "wwwroot", "Images", newFileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                await image.CopyToAsync(fileStream); 
+            }
+
+            return Ok($"Images/{newFileName}");
+        }
+
+    }
+}
---
+++ BlazorFiles.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Blazor.FileReader;
+
+namespace BlazorFiles.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddFileReaderService(options =>
+            {
+                options.UseWasmSharedBuffer = true;
+            });
+
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
